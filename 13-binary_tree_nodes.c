df0522ff5a0e044508ef9c3140c2813087c8d8f7
Colin <clnearle@gmail.com>
2023-11-29 03:59:41 -0800

    task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - couints teh amount of nodes
+ * @tree: pointer to root'
+ * Return: ammount of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_node(tree->right));
+	}
+	return (0);
+}
