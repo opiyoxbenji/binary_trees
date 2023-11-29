6dbca3fbbb45338a005befb9168e1ccea58f583b
Colin <clnearle@gmail.com>
2023-11-29 10:50:08 -0800

    task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,17 +9,50 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
 	const binary_tree_t *temp1, *temp2;
+	size_t depth1, depth2;
 
 	if (!first || !second)
 	{
 		return (NULL);
 	}
+	depth1 = binary_tree_depth(first);
+	depth2 = binary_tree_depth(second);
 	temp1 = first;
 	temp2 = second;
-	while (temp1 != temp2)
+	while (depth1 > depth2)
 	{
-		temp1 = temp1->parent ? temp1->parent : second;
-		temp2 = temp2->parent ? temp2->parent : first;
+		temp1 = temp1->parent;
+		depth1--;
 	}
-	return ((binary_tree_t *)temp1);
+	while (depth2 > depth1)
+	{
+		temp2 = temp2->parent;
+		depth2--;
+	}
+	while (temp1 && temp2)
+	{
+		if (temp1 == temp2)
+		{
+			return ((binary_tree_t *)temp1);
+		}
+		temp1 = temp1->parent;
+		temp2 = temp2->parent;
+	}
+	return (NULL);
+}
+/**
+ * binary_tree_depth - measures depth of node
+ * @tree: pointer to root
+ * Return: depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree && tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
