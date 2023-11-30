8e788fc83036fb21a7a18f174f8b1b021fff7fcf
Colin <clnearle@gmail.com>
2023-11-30 09:23:14 -0800

    task 28

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -7,16 +7,16 @@
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-	if (tree == NULL || tree->n == value)
+	if (tree != NULL)
 	{
-		return (tree);
+		if (tree->n == value)
+		{
+			return ((bst_t *)tree);
+		}
+		if (tree->n > value)
+		{
+			return (bst_search(tree->right, value));
+		}
 	}
-	if (value < tree->n)
-	{
-		return (bst_search(tree->left, value));
-	}
-	else
-	{
-		return (bst_search(tree->right, value));
-	}
+	return (NULL);
 }
